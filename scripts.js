d725ba6ec2e56c30e77366048bbf28092e465a6e
José Anderson <jose.andersonjr2@gmail.com>
2022-09-19 09:28:05 -0300

    Tratamento de erros e finzalização do projeto

--- scripts.js
+++ scripts.js
@@ -1,25 +1,49 @@
-// sintaxe
-function primeiraFuncao(){
+// // sintaxe
+// function primeiraFuncao(){
 
-    return new Promise((resolve) => {
+//     return new Promise((resolve) => {
 
-        setTimeout(() => {
-            console.log('Esperou isso aqui')
-            resolve()//Resolvo a promise pra realizar o fluxo dela corretamente
-        }, 1000)
+//         setTimeout(() => {
+//             console.log('Esperou isso aqui')
+//             resolve()//Resolvo a promise pra realizar o fluxo dela corretamente
+//         }, 1000)
 
-    })
+//     })
+
+// }
+
+// async function segundaFuncao() {
+
+//     console.log('Iniciou')
+
+//     await primeiraFuncao()
+
+//     console.log('Terminou')
+    
+// } 
+
+// segundaFuncao()
+
+//Prático 
+function getUser(id) {
+
+    return fetch(`https://reqres.in/api/users?id=${id}`)
+        .then(data => data.json())
+        .catch(erro => console.log(erro))
 
 }
 
-async function segundaFuncao() {
+async function showUserName(id) {
+    // try e catch para tratamento de erros
+    try {
+        const user = await getUser(id)
 
-    console.log('Iniciou')
-
-    primeiraFuncao()
-
-    console.log('Terminou')
+        console.log(`O nome do usuário é: ${user.data.first_name}`)
     
-} 
+    } catch(erro) {
+            console.log(`Erro: ${erro}`)
+    }
 
-segundaFuncao()
+}
+
+showUserName(3)
